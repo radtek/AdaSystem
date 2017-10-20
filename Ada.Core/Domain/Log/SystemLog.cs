7828cc2612b621ea043079fcbc512048020d8fbc
熊琦(Home) <ada.xiong@live.cn>
2017-10-20 20:42:21 +0800

    Bug

---
+++ Ada.Core/Domain/Log/SystemLog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ada.Core.Domain.Log
+{
+    public class SystemLog
+    {
+        /// <summary>
+        /// 日志主键
+        /// </summary>
+        [Display(Name = "主键")]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// 日志时间
+        /// </summary>
+        [Display(Name = "日志时间")]
+        public System.DateTime? Date { get; set; }
+
+        /// <summary>
+        /// 线程编号
+        /// </summary>
+        [Display(Name = "线程编号")]
+        public string Thread { get; set; }
+
+        /// <summary>
+        /// 日志级别
+        /// </summary>
+        [Display(Name = "日志级别")]
+        public string Level { get; set; }
+
+        /// <summary>
+        /// 日志模块
+        /// </summary>
+        [Display(Name = "日志模块")]
+        public string Logger { get; set; }
+
+        /// <summary>
+        /// 日志描述
+        /// </summary>
+        [Display(Name = "日志描述")]
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 异常信息
+        /// </summary>
+        [Display(Name = "异常信息")]
+        public string Exception { get; set; }
+    }
+}
---
+++ Ada.Data/Mapping/Log/SystemLogMap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ada.Core.Domain.Admin;
+using Ada.Core.Domain.Log;
+
+namespace Ada.Data.Mapping.Log
+{
+   public class SystemLogMap : EntityTypeConfiguration<SystemLog>
+    {
+        public SystemLogMap()
+        {
+            //配置主键
+            HasKey(s => s.Id);
+
+            ////给ID配置自动增长
+            this.Property(s => s.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            //配置字段
+            Property(s => s.Level).HasMaxLength(32);
+            Property(s => s.Thread).HasMaxLength(32);
+            Property(s => s.Date);
+            Property(s => s.Logger).HasMaxLength(128);
+            Property(s => s.Message);
+            Property(s => s.Exception);
+            
+
+            //配置表
+            ToTable("SystemLog");
+
+            
+        }
+    }
+}
