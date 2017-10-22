6a21ca755b2269d1067c5a963f0a513c79427d7e
熊琦 <ada.xiong@live.cn>
2017-10-22 23:41:28 +0800

    完善后台

---
+++ Ada.Core/Domain/Log/ManagerLoginLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ada.Core.Domain.Admin
+{
+   public class ManagerLoginLog:BaseEntity
+    {
+        /// <summary>
+        /// 登陆时间
+        /// </summary>
+        [Display(Name = "登陆时间")]
+        public System.DateTime? LoginTime { get; set; }
+        /// <summary>
+        /// 登陆信息
+        /// </summary>
+        [Display(Name = "登陆信息")]
+        public string WebInfo { get; set; }
+        /// <summary>
+        /// 用户
+        /// </summary>
+        [Display(Name = "用户")]
+        public string ManagerId { get; set; }
+
+        public virtual Manager Manager { get; set; }
+    }
+}
---
+++ Ada.Data/Mapping/Log/ManagerLoginLogMap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ada.Core.Domain.Admin;
+
+namespace Ada.Data.Mapping.Log
+{
+    public class ManagerLoginLogMap : EntityTypeConfiguration<ManagerLoginLog>
+    {
+        public ManagerLoginLogMap()
+        {
+            //配置主键
+            HasKey(s => s.Id);
+
+            ////给ID配置自动增长
+            //this.Property(s => s.ID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            //配置字段
+
+            Property(s => s.LoginTime);
+            Property(s => s.ManagerId).HasMaxLength(128);
+            Property(s => s.WebInfo).HasMaxLength(512);
+
+            Property(s => s.AddedDate);
+            Property(s => s.AddedBy).HasMaxLength(32);
+            Property(s => s.ModifiedDate);
+            Property(s => s.ModifiedBy).HasMaxLength(32);
+            Property(s => s.IsDelete).IsRequired();
+            Property(s => s.DeletedDate);
+            Property(s => s.DeletedBy).HasMaxLength(32);
+            Property(s => s.IpAddress).HasMaxLength(32);
+            Property(s => s.Taxis);
+            Property(s => s.Remark).HasMaxLength(1024);
+
+            //配置表
+            ToTable("ManagerLoginLog");
+            //配置关系【一对多的配置，外键是UserId】 Withmany方法允许多个。HasForeignKey方法表示哪个属性是User表的外键，WillCascadeOnDelete方法用来配置是否级联删除
+            HasRequired(s => s.Manager).WithMany(s => s.ManagerLoginLogs).HasForeignKey(s => s.ManagerId).WillCascadeOnDelete(true);
+        }
+    }
+}
