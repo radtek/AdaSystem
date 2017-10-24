313e26bd4c8d1cdfca1b7d1b86339f88e809302d
熊琦 <ada.xiong@live.cn>
2017-10-24 18:14:53 +0800

    jstree

---
+++ Ada.Web/Areas/Admin/Controllers/ActionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ada.Framework.Filter;
+
+namespace Admin.Controllers
+{
+    public class ActionController : BaseController
+    {
+        // GET: Action
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update()
+        {
+            return Json(null);
+        }
+    }
+}
