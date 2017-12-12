88d3ae6fd8b5c357b9c0d1216798c2f7ca035221
熊琦 <ada.xiong@live.cn>
2017-12-12 18:43:44 +0800

    WGLH

---
+++ Ada.Core/Domain/Log/WorkLog.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ada.Core.Domain.Log
+{
+    /// <summary>
+    /// 工作日志
+    /// </summary>
+    public class WorkLog : BaseEntity
+    {
+        /// <summary>
+        /// 日志时间
+        /// </summary>
+        [Display(Name = "日志时间")]
+        public System.DateTime? Date { get; set; }
+
+        /// <summary>
+        /// 工作主题
+        /// </summary>
+        [Display(Name = "工作主题")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 工作内容
+        /// </summary>
+        [Display(Name = "工作内容")]
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 主管寄语
+        /// </summary>
+        [Display(Name = "主管寄语")]
+        public string Manager { get; set; }
+
+        /// <summary>
+        /// 总监寄语
+        /// </summary>
+        [Display(Name = "总监寄语")]
+        public string Director { get; set; }
+
+        /// <summary>
+        /// BOSS寄语
+        /// </summary>
+        [Display(Name = "BOSS寄语")]
+        public string Boss { get; set; }
+        /// <summary>
+        /// 日志人员
+        /// </summary>
+        [Display(Name = "日志人员")]
+        public string Transactor { get; set; }
+        /// <summary>
+        /// 日志人员
+        /// </summary>
+        [Display(Name = "日志人员")]
+        public string TransactorId { get; set; }
+    }
+}
---
+++ Ada.Data/Mapping/Log/WorkLogMap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ada.Core.Domain.Admin;
+using Ada.Core.Domain.Log;
+
+namespace Ada.Data.Mapping.Log
+{
+    public class WorkLogMap : EntityTypeConfiguration<WorkLog>
+    {
+        public WorkLogMap()
+        {
+            //配置主键
+            HasKey(s => s.Id);
+
+            
+            Property(s => s.Title).HasMaxLength(128);
+            Property(s => s.Transactor).HasMaxLength(32);
+            Property(s => s.TransactorId).HasMaxLength(32);
+
+            Property(s => s.AddedBy).HasMaxLength(32);
+            Property(s => s.AddedById).HasMaxLength(32);
+            Property(s => s.ModifiedBy).HasMaxLength(32);
+            Property(s => s.ModifiedById).HasMaxLength(32);
+            Property(s => s.IsDelete).IsRequired();
+            Property(s => s.DeletedBy).HasMaxLength(32);
+            Property(s => s.DeletedById).HasMaxLength(32);
+            Property(s => s.IpAddress).HasMaxLength(32);
+            Property(s => s.Remark).HasMaxLength(1024);
+
+            //配置表
+            ToTable("WorkLog");
+
+        }
+    }
+}
